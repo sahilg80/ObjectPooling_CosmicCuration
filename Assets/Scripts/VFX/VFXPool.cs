4c293e3064bdc4f921934482b473c96ccb105f3b
sahilg80 <38989332+sahilg80@users.noreply.github.com>
2024-03-29 21:36:48 +0530

    Added vfx pool class

---
+++ Assets/Scripts/VFX/VFXPool.cs
@@ -0,0 +1,32 @@
+﻿using CosmicCuration.Utilities;
+using CosmicCuration.VFX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.VFX
+{
+    public class VFXPool : GenericObjectPool<VFXController>
+    {
+        private List<VFXData> vfxData;
+
+        public VFXPool(List<VFXData> vfxData)
+        {
+            this.vfxData = vfxData;
+        }
+
+        public VFXController GetVFXFromPool<T>(VFXType type)
+        {
+            VFXView prefabToSpawn = vfxData.Find(item => item.type == type).prefab;
+            return GetItem<T>();
+        }
+
+        protected override VFXController CreateItem<T>()
+        {
+            
+        }
+
+    }
+}
--- Assets/Scripts/VFX/VFXService.cs
+++ Assets/Scripts/VFX/VFXService.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts.VFX;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,8 +7,13 @@ namespace CosmicCuration.VFX
     public class VFXService
     {
         private List<VFXData> vfxData = new List<VFXData>();
+        private VFXPool vFXPool;
 
-        public VFXService(VFXScriptableObject vfxScriptableObject) => vfxData = vfxScriptableObject.vfxData;
+        public VFXService(VFXScriptableObject vfxScriptableObject)
+        {
+            vfxData = vfxScriptableObject.vfxData;
+            vFXPool = new VFXPool(vfxData);
+        }
 
         public void PlayVFXAtPosition(VFXType type, Vector2 spawnPosition)
         {
